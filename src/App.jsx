f5b31b1e78bddfe294165427f613787dfdbfb810
Crispin Cirhuza Zagabe <celiitellu@gmail.com>
2021-09-01 20:20:18 +0200

    Feat: Set the skeleton of the signin form

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,52 @@ function App() {
                 <span className="hero-foot">Transférer votre argent en toute sécurité.</span>
             </div>
             <div className="app-body">
-                <a href="#">ENROLLEMENT</a>
-                <p>Au cas où vous avez déjà un compte actif et vérifié <br /> alors connectez-vous</p>
-                <a href="#">CONNEXION</a>
+                <div className="left-block">
+
+                </div>
+                <div className="body-content">
+                    <form className="signin-form">
+                        <div className="input-field">
+                            <label htmlFor="first-name">Prénom</label>
+                            <input type="text" name="fist-name" id="first-name" />
+                        </div>
+                        <div className="input-field">
+                            <label htmlFor="second-name">Nom</label>
+                            <input type="text" name="second-name" id="second-name" />
+                        </div>
+                        <div className="input-field">
+                            <label htmlFor="third-name">Post-nom</label>
+                            <input type="text" name="third-name" id="third-name" />
+                        </div>
+                        <div className="input-field">
+                            <label htmlFor="num-visa">Num. Carte Visa</label>
+                            <input type="number" name="num-visa" id="num-visa" />
+                        </div>
+                        <div className="input-field">
+                            <label htmlFor="num-visa-dest">Num. Carte Visa destinataire</label>
+                            <input type="number" name="num-visa-dest" id="num-visa-dest" />
+                        </div>
+                        <div className="input-field">
+                            <label htmlFor="devise">Devise</label>
+                            <select name="devises" id="devises">
+                                <option value="fc">Franc Congolais</option>
+                                <option value="ussd">Dollar US</option>
+                                <option value="euro">Franc Euro</option>
+                            </select>
+                        </div>
+                        <div className="input-field">
+                            <label htmlFor="amount">Montant</label>
+                            <input type="number" name="amount" id="amount" />
+                        </div>
+                        <div className="input-field">
+                            <input type="checkbox" name="agree" id="agree" />
+                            <p>En soumettant ce transfert, vous agréer avec notre <a href="/politics">politique de transfert des fonds</a></p>
+                        </div>
+                        <div className="input-field">
+                            <input type="submit" value="Transfert" />
+                        </div>
+                    </form>
+                </div>
             </div>
             <div className="footer">
                 <ul className="about-block">
